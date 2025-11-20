db51fef870d9af0807e5eed217b443f20e7a2b71
Ahmed Elsayed <ahmeding.hosam@gmail.com>
2025-11-20 00:47:20 +0000

    fix(backend): add DELETE to the list of allowed methods

--- backend/main.go
+++ backend/main.go
@@ -29,7 +29,7 @@ func main() {
 func toDoListHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE")
 
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
